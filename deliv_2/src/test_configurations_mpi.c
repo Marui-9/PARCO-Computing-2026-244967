7e7d39588ecef29e507e74e49f0f25cd81299a98
Marui-9 <marchesin.jacopo772@gmail.com>
2026-01-09 09:17:08 +0100

    24 threads

--- deliv_2/src/test_configurations_mpi.c
+++ deliv_2/src/test_configurations_mpi.c
@@ -122,7 +122,7 @@ int main(int argc, char* argv[]) {
 
     const char *matrix_file = argv[1];
     int num_iterations = (argc == 3) ? atoi(argv[2]) : 50;
-    const int thread_count = 48;  // Fixed optimal thread count per NUMA analysis
+    const int thread_count = 24;  // Fixed optimal thread count per NUMA analysis
     
     fprintf(stderr, "Rank %d: Matrix file arg: %s\n", global_rank, matrix_file);
     fflush(stderr);
